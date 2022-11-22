4bc28baa1816de5fa37ae92f0d9573eff31d9031
jamesvovos <james.vovos@gmail.com>
2022-11-22 17:47:40 +1100

    Fixed up navigation bar styling.

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -1,14 +1,12 @@
-import { BsFillMoonStarsFill } from "react-icons/bs";
-
 export default function Navbar() {
   return (
     <div>
       <nav className="py-10 mb-12 flex justify-between">
-        <h1 className="text-xl font-burtons">developedbyJames</h1>
+        <h1 className="text-xl font-burtons">
+          <span className="text-customblue-50 text-3xl">&lt;/&gt; </span>
+          developedbyJames
+        </h1>
         <ul className="flex items-center">
-          <li>
-            <BsFillMoonStarsFill className="cursor-pointer" />
-          </li>
           <li>
             <a
               className="bg-customblue-50 text-white px-4 py-2 rounded-md ml-8"
