05f7d7273b6dbff282af86f8ef3eafc8bc65c241
jamesvovos <james.vovos@gmail.com>
2023-07-26 12:02:56 +1000

    removed css styling and replaced with tailwind css, fixed mobile resizing issue.

--- pages/components/techStack.tsx
+++ pages/components/techStack.tsx
@@ -1,49 +1,23 @@
-import Image from "next/image";
 import React from "react";
+import { DiReact, DiPython } from "react-icons/di";
 
 import dynamic from "next/dynamic";
 const ReactTooltip = dynamic(() => import("react-tooltip"), { ssr: false });
-
-import react from "../../public/assets/images/tech/react-logo-svg-blue.png";
-import aws from "../../public/assets/images/tech/aws-amplify-logo-svg-blue.png";
-import python from "../../public/assets/images/tech/python.png";
-
-const data = [
-  {
-    id: 1,
-    logo: react,
-    alt: "React Logo",
-  },
-  {
-    id: 2,
-    logo: python,
-    alt: "Python Logo",
-  },
-];
 
 export default function TechStack() {
   return (
     <section id="text-center min-h-screen">
       <div className="flex justify-center pt-20">
-        <div className="relative text-4xl flex justify-center gap-16 py-5 px-2 text-white font-poppins">
+        <div className="relative text-4xl flex justify-center text-white font-poppins">
           <p>Tech Stack |</p>
+          <span className="text-customblue-50">
+            <DiReact className="cursor-pointer" data-tip="React/React Native" />
+          </span>{" "}
+          <span className="text-customblue-50">
+            <DiPython className="cursor-pointer" data-tip="Python" />
+          </span>{" "}
+          <ReactTooltip />
         </div>
-        {data.map(({ id, logo, alt }) => {
-          return (
-            <div key={id} className="justify-center gap-16 py-5">
-              <div className="techStackIcon md:w-42 lg:w-42">
-                <Image
-                  data-tip={alt}
-                  src={logo}
-                  alt={alt}
-                  className="icon"
-                  priority={true}
-                />
-                <ReactTooltip />
-              </div>
-            </div>
-          );
-        })}
       </div>
     </section>
   );
