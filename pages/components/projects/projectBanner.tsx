8843568516a22e945f0a7089ecaf042ee60f4d2e
jamesvovos <james.vovos@gmail.com>
2023-07-26 18:11:28 +1000

    added media queries for mobile and tablet for project page.

--- pages/components/projects/projectBanner.tsx
+++ pages/components/projects/projectBanner.tsx
@@ -3,12 +3,13 @@ import ProjectVideo from "../projectVideo";
 
 export default function ProjectBanner() {
   return (
-    <div className="text-left container mx-auto flex-wrap grid grid-cols-2 gap-4 my-10">
-      <div className="col-span-1">
-        <h2 className="relative text-8xl py-2 text-customblue-50 font font-poppinsbold">
+    <div className="text-left container mx-auto flex-wrap grid grid-cols-1 md:grid-cols-2 gap-4 my-10">
+      {/* On small and medium screens (col-span-1) */}
+      <div className="md:col-span-1">
+        <h2 className="relative text-4xl md:text-6xl py-2 text-customblue-50 font-poppinsbold">
           CLOCS-A
         </h2>
-        <h3 className="relative text-l py-2 text-black font-poppins">
+        <h3 className="relative text-base md:text-lg py-2 text-black font-poppins">
           Final Year Capstone project in collaboration with Transport NSW &
           CLOCS-A. Initiative to develop several-cutting edge mobile apps that
           help construction and logistics companies across Australia monitor and
@@ -17,8 +18,10 @@ export default function ProjectBanner() {
           apps.
         </h3>
       </div>
-      <div className="col-span-1">
-        <div className="relative text-5xl flex justify-left gap-16 py-3 text-gray-600 bg-charcoal-100 rounded">
+
+      {/* On small and medium screens (col-span-1) */}
+      <div className="md:col-span-1">
+        <div className="relative text-3xl md:text-4xl flex justify-left gap-8 py-3 text-gray-600 bg-charcoal-100 rounded">
           <ProjectVideo
             url="https://www.youtube.com/watch?v=zbYBgy_ChGY"
             thumbnail=""
--- pages/components/projects/projectTechStack.tsx
+++ pages/components/projects/projectTechStack.tsx
@@ -6,26 +6,20 @@ const ReactTooltip = dynamic(() => import("react-tooltip"), { ssr: false });
 
 export default function ProjectTechStack() {
   return (
-    <section id="text-center min-h-screen">
-      <div className="container mx-auto grid grid-cols-2 pb-20">
-        <div className="relative text-4xl flex justify-center text-black font-poppinsbold"></div>
-        <div className="text-left">
-          <div className="relative text-4xl flex justify-left text-black font-poppinsbold">
-            <p>Tech Stack |</p>
-            <span className="text-customblue-50">
-              <SiReact
-                className="cursor-pointer"
-                data-tip="React/React Native"
-              />
-            </span>{" "}
-            <span className="text-customblue-50">
-              <SiAwsamplify className="cursor-pointer" data-tip="AWS Amplify" />
-            </span>{" "}
-            <span className="text-customblue-50">
-              <SiGraphql className="cursor-pointer" data-tip="GraphQL" />
-            </span>{" "}
-            <ReactTooltip />
-          </div>
+    <section className="text-center">
+      <div className="container mx-auto flex justify-end pb-20">
+        <div className="relative text-xl md:text-4xl flex justify-left text-black font-poppinsbold">
+          <p>Tech Stack |</p>
+          <span className="text-customblue-50">
+            <SiReact className="cursor-pointer" data-tip="React/React Native" />
+          </span>{" "}
+          <span className="text-customblue-50">
+            <SiAwsamplify className="cursor-pointer" data-tip="AWS Amplify" />
+          </span>{" "}
+          <span className="text-customblue-50">
+            <SiGraphql className="cursor-pointer" data-tip="GraphQL" />
+          </span>{" "}
+          <ReactTooltip />
         </div>
       </div>
     </section>
