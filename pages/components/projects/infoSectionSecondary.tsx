caa1a1611a25001ed621caac333b3b82ae351a90
jamesvovos <james.vovos@gmail.com>
2023-07-27 11:18:42 +1000

    removed css for projects page and replaced with tailwind css. cleaned up unused code.

--- pages/components/projects/infoSectionSecondary.tsx
+++ pages/components/projects/infoSectionSecondary.tsx
@@ -10,20 +10,21 @@ type InfoSectionProps = {
 
 export default function InfoSectionSecondary(props: InfoSectionProps) {
   return (
-    <section id="infoSecondary" className="container mx-auto">
-      <div className="projectInfoCard">
-        <div className="infoCard bg-charcoal-100">
-          <div className="player-wrapper">
-            <ProjectVideo url={props.video} thumbnail={props.thumbnail} />
-          </div>
-          <div className="infoCardTextWrapperSecondary">
-            <div className="infoCardTitleSecondary">
-              <h3>{props.title}</h3>
-            </div>
+    <section id="infoSectionSecondary" className="container mx-auto m-10">
+      <div className="bg-charcoal-100 flex flex-col md:flex-row text-center md:text-left rounded-xl">
+        {/* Video on larger screens */}
+        <div className="md:w-1/2 order-last md:order-first py-5 px-5">
+          <ProjectVideo url={props.video} thumbnail={props.thumbnail} />
+        </div>
 
-            <div className="infoCardDescSecondary">
-              <p>{props.desc}</p>
-            </div>
+        {/* Title and description */}
+        <div className="md:w-1/2 md:flex md:flex-col md:text-center lg:text-right px-5 py-5">
+          <div className="relative text-4xl md:text-6xl py-2 text-customblue-50 font-poppinsbold">
+            <h3>{props.title}</h3>
+          </div>
+
+          <div className="relative text-base md:text-l lg:text-l xl:text-lg text-white font-poppins">
+            <p>{props.desc}</p>
           </div>
         </div>
       </div>
