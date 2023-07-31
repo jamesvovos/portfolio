3894495dc112d87378a900ad81ef973c8ecc370a
jamesvovos <james.vovos@gmail.com>
2023-07-31 10:35:16 +1000

    testing contain objectFit property on infoSection components.

--- pages/components/projects/infoSectionSecondary.tsx
+++ pages/components/projects/infoSectionSecondary.tsx
@@ -16,7 +16,7 @@ export default function InfoSectionSecondary(props: InfoSectionProps) {
           <Image
             src={props.image}
             alt={props.title}
-            style={{ objectFit: "scale-down", width: "100%", height: "100%" }}
+            style={{ objectFit: "contain", width: "100%", height: "100%" }}
           />
         </div>
 
